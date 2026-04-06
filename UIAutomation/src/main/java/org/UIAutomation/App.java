eadf0a4d6c42df8ca7854a4eb6cd1447bb3fcd99
sailubandi <sailubandi33@gmail.com>
2026-04-06 14:55:35 +0530

    Initial commit

---
+++ UIAutomation/src/main/java/org/UIAutomation/App.java
@@ -0,0 +1,13 @@
+package org.UIAutomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
