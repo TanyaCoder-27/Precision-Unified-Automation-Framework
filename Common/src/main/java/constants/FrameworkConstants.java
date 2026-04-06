54def6bce5306b833dad0b2355289a2ccb1e579b
Rihitha <harisubrahmanyam696@gmail.com>
2026-04-06 15:26:35 +0530

    common folder commit

---
+++ Common/src/main/java/constants/FrameworkConstants.java
@@ -0,0 +1,13 @@
+package constants;
+
+public final class FrameworkConstants {
+
+    private FrameworkConstants() {}
+
+    public static final String ROOT_PATH = System.getProperty("user.dir");
+
+    public static final String CONFIG_FILE_PATH =
+    	"D:\\VeevaProject\\hybridAutomation\\Common\\src\\main\\resources\\config.properties";
+
+    public static final int EXPLICIT_WAIT = 10;
+}
