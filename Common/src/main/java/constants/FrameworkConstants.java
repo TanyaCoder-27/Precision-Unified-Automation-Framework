dce0163618afa7a23fd26a97084d4d67e7d2f2f9
Rihitha <harisubrahmanyam696@gmail.com>
2026-04-06 16:13:25 +0530

    register new user

--- Common/src/main/java/constants/FrameworkConstants.java
+++ Common/src/main/java/constants/FrameworkConstants.java
@@ -7,7 +7,8 @@ public final class FrameworkConstants {
     public static final String ROOT_PATH = System.getProperty("user.dir");
 
     public static final String CONFIG_FILE_PATH =
-    	"D:\\VeevaProject\\hybridAutomation\\Common\\src\\main\\resources\\config.properties";
+    		System.getProperty("user.dir") 
+            + "/../Common/src/main/resources/config.properties";
 
     public static final int EXPLICIT_WAIT = 10;
 }
